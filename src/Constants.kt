d4147bc626abc9d19b9620d9ee5ac6da863fa67a
tushar kokane <tushar.kokane@walmartlabs.com>
2021-09-19 20:02:09 +0530

    Initial commit - Initial development for AndroidSearchIt plugin

---
+++ src/Constants.kt
@@ -0,0 +1,7 @@
+object Constants {
+    const val NOTIFICATION_GROUP = "Android Search It Notifications"
+    const val GOOGLE = "google"
+    const val STACK_OVERFLOW = "stackoverflow"
+    const val GOOGLE_LINK = "https://www.google.com"
+    const val STACK_OVERFLOW_LINK = "https://www.stackoverflow.com"
+}
---
+++ src/SearchKeyword.kt
@@ -0,0 +1,75 @@
+import com.intellij.ide.BrowserUtil
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Editor
+
+/**
+ * Extension of [AnAction].
+ *
+ * It takes the incoming parameter from the event and launches the browser with appropriate
+ * search engine.
+ */
+class SearchKeyword: AnAction() {
+
+    override fun actionPerformed(actionEvent: AnActionEvent) {
+        val editor = getEditor(actionEvent)
+        if (editor?.selectionModel?.hasSelection() == true) {
+            var success = false
+            when(actionEvent.presentation.text.lowercase()) {
+                Constants.GOOGLE -> {
+                    success = launchBrowser(Constants.GOOGLE_LINK, getSearchKeyword(actionEvent))
+                }
+                Constants.STACK_OVERFLOW -> {
+                    success = launchBrowser(Constants.STACK_OVERFLOW_LINK, getSearchKeyword(actionEvent))
+                }
+            }
+
+            if(!success) showErrorNotification(actionEvent)
+        } else {
+            showErrorNotification(actionEvent)
+        }
+    }
+
+    /**
+     * Since our plugin is editor dependent, enable the plugin only if an editor is open and some text is
+     * initially selected.
+     */
+    override fun update(actionEvent: AnActionEvent) {
+        actionEvent.presentation.isEnabledAndVisible = actionEvent.project != null && !getEditor(actionEvent)?.caretModel?.allCarets.isNullOrEmpty()
+    }
+
+    /**
+     * Get [Editor] instance for current [AnAction].
+     *
+     * @return [Editor] instance.
+     */
+    private fun getEditor(actionEvent: AnActionEvent): Editor? {
+        return actionEvent.getData(CommonDataKeys.EDITOR)
+    }
+
+    /**
+     * In case no text is selected and user tries to use the plugin, show this error notification.
+     *
+     * Note:- Need to update this implementation to be compatible with future versions.
+     */
+    private fun showErrorNotification(actionEvent: AnActionEvent) {
+        NotificationGroup(Constants.NOTIFICATION_GROUP, NotificationDisplayType.BALLOON, true)
+            .createNotification(Constants.NOTIFICATION_GROUP, "No text is selected. Please select some text to look up.", NotificationType.INFORMATION, null)
+            .notify(actionEvent.project)
+    }
+
+    private fun launchBrowser(baseUrl: String, searchKeyword: String?): Boolean {
+        if (searchKeyword.isNullOrEmpty()) return false
+
+        BrowserUtil.browse("$baseUrl/search?q=android $searchKeyword")
+        return true
+    }
+
+    private fun getSearchKeyword(actionEvent: AnActionEvent): String? {
+        return getEditor(actionEvent)?.caretModel?.currentCaret?.selectedText
+    }
+}
