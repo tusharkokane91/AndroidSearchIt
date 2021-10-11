e14e815a81720505f8d57bea2d26cc273cb02f32
Tushar Kokane <tushar.kokane@walmart.com>
2021-10-11 17:50:05 +0530

    Added medium support

--- src/Constants.kt
+++ src/Constants.kt
@@ -2,6 +2,8 @@ object Constants {
     const val NOTIFICATION_GROUP = "Android Search It Notifications"
     const val GOOGLE = "google"
     const val STACK_OVERFLOW = "stackoverflow"
+    const val MEDIUM = "medium"
     const val GOOGLE_LINK = "https://www.google.com"
     const val STACK_OVERFLOW_LINK = "https://www.stackoverflow.com"
+    const val MEDIUM_LINK = "https://medium.com"
 }
